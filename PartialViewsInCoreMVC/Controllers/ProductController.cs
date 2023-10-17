e4f621e7a070bea4f9734217c2639e3017b081aa
mukesh kumar <mukesh.1006jhr@outlook.com>
2023-10-18 04:15:59 +0530

    Add project files.

---
+++ PartialViewsInCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsInCoreMVC.Models;
+
+namespace PartialViewsInCoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private List<Product> products = new List<Product>();
+        public ProductController()
+        {
+            products = new List<Product>()
+                {
+                     new Product { ProductID =1, Name ="Product 1", Category = "Category 1", Description ="Description 1", Price = 10m},
+                new Product { ProductID =2, Name ="Product 2", Category = "Category 1", Description ="Description 2", Price = 20m},
+                new Product { ProductID =3, Name ="Product 3", Category = "Category 1", Description ="Description 3", Price = 30m},
+                new Product { ProductID =4, Name ="Product 4", Category = "Category 2", Description ="Description 4", Price = 40m},
+                new Product { ProductID =5, Name ="Product 5", Category = "Category 2", Description ="Description 5", Price = 50m},
+                new Product { ProductID =6, Name ="Product 6", Category = "Category 2", Description ="Description 6", Price = 50m}
+                };
+        }
+        public ActionResult Index()
+        {
+            return View(products);      
+        }
+
+        public ActionResult Details(int id)
+        {
+            var productDetails = products.FirstOrDefault(p => p.ProductID == id);
+            return View(productDetails);
+        }
+    }
+}
---
+++ PartialViewsInCoreMVC/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace PartialViewsInCoreMVC.Models
+{
+    public class Product
+    {
+        public long ProductID { get; set; }
+        public string? Name { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+}
